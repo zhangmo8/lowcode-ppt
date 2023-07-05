40a315a9f7be8448ad54b4887df0b9f113c04cf3
Lei Wang <lei.wang1@thoughtworks.com>
2023-07-05 23:45:14 +0800

    feat: update slide

---
+++ components/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div border-rd border-1 p-1 mr-1>
+    <header flex border-rd border-1 px-2 py-1 justify-between m-1>
+      <div>Header Left</div>
+      <div>Header Center</div>
+      <div>Header Right</div>
+    </header>
+    <main border-rd border-1 p-1 flex m-1 h-xs>
+      <div border-rd border-1 border-dashed m-1 p-1 flex flex-col justify-between>
+        <div>Sidbar Top</div>
+        <div>Sidbar Bottom</div>
+      </div>
+      <div w-xl border-rd border-1 border-dashed m-1 p-1 text-center>Designer</div>
+      <div flex-1 border-rd border-1 border-dashed m-1 p-1 text-center>Setters</div>
+    </main>
+  </div>
+</template>
